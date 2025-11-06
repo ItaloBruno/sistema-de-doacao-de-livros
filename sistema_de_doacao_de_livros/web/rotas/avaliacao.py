10c2a63a00cd50393ee51e6df8468d64cfd2e5ef
Rafael Coelho <rafaellcoellho@gmail.com>
2025-11-06 20:00:00 -0300

    feat: Implementa protótipo do sistema sem banco de dados

--- sistema_de_doacao_de_livros/web/rotas/avaliacao.py
+++ sistema_de_doacao_de_livros/web/rotas/avaliacao.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-rotas_solicitacoes = APIRouter()
+rotas_solicitacoes = APIRouter(include_in_schema=False)
 
 templates = Jinja2Templates(
     directory="sistema_de_doacao_de_livros/web/templates"
--- sistema_de_doacao_de_livros/web/rotas/inicio.py
+++ sistema_de_doacao_de_livros/web/rotas/inicio.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-rotas_inicio = APIRouter()
+rotas_inicio = APIRouter(include_in_schema=False)
 
 templates = Jinja2Templates(
     directory="sistema_de_doacao_de_livros/web/templates"
